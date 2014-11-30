d1d611b7e1bb49038be2bc27e9c0ae97f695df5e
Evan A <evjay.a@gmail.com>
2014-11-29 21:10:50 -0700

    set up to delete posts

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -20,8 +20,25 @@ class PostController < ApplicationController
   		render.new
   	end
   end
+
   def edit
     @post = Post.find(params[:id])
+  end
+
+  def update
+    @post - Post.find(params[:id])
+
+    if @post.update_attributes(posts_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
 
   private
