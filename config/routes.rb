e397e6c57d7741ab08b7d13dab9ea8672074ad78
Evan A <evjay.a@gmail.com>
2014-11-29 19:47:39 -0700

    Gererated controller at Post index show new

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'post/index'
+
+  get 'post/show'
+
+  get 'post/new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
